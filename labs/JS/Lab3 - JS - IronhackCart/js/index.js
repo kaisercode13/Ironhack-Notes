a4a42b31be8b0c71b49685bf88455a614218daa3
kaisercode13 <alex.reinhardt22@gmail.com>
2022-11-17 12:32:44 +0100

    ironhack cart lab update

--- labs/JS/Lab3 - JS - IronhackCart/js/index.js
+++ labs/JS/Lab3 - JS - IronhackCart/js/index.js
@@ -4,29 +4,42 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
-}
+  let price, quantity, subtotal;
+  price = product.querySelector('.price span').innerText;
+  quantity = product.querySelector('.quantity input').value;
+  subtotal = price * quantity;
+
+  product.querySelector('.subtotal span').innerText = subtotal;
+
+  return subtotal;
+};
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  let products = [...document.querySelectorAll('.product')];
+  products.forEach((product) => updateSubtotal(product))
 
   // ITERATION 3
   //... your code goes here
-}
+  let total, totalDOM;
+
+  products = Array.from(document.querySelectorAll('.product'));
+  totalDOM = document.querySelector('#total-value > span');
+  total = 0;
+
+  products.forEach((product) => total+=updateSubtotal(product));
+  totalDOM.innerHTML = total; 
+};
 
 // ITERATION 4
-
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
 
 // ITERATION 5
 
