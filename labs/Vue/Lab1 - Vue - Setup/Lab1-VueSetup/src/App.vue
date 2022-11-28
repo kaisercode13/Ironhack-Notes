54a2d74e18434eb5d00ab4448f6d194d8a5b6029
kaisercode13 <alex.reinhardt22@gmail.com>
2022-11-28 13:45:13 +0100

    vue main update

--- labs/Vue/Lab1 - Vue - Setup/Lab1-VueSetup/src/App.vue
+++ labs/Vue/Lab1 - Vue - Setup/Lab1-VueSetup/src/App.vue
@@ -1,43 +1,38 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import MyFirstComponentVue from "./components/vueBasics/MyFirstComponent.vue";
-import TextDirective from "./components/vueBasics/2-TextDirective.vue";
-import HtmlDirective from "./components/vueBasics/3-HtmlDirective.vue";
+import Lab1Component from './components/Lab1Component.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="Vue Basics Class" />
+      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
-  <main>
-    <!-- <MyFirstComponentVue /> -->
-    <hr />
-    <TextDirective />
-    <hr />
-    <HtmlDirective />
-  </main>
+  <main></main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
 }
+
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
+
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
+
   .logo {
     margin: 0 2rem 0 0;
   }
+
   header .wrapper {
     display: flex;
     place-items: flex-start;
