89f49ed2f1fa7b4ed993dff54537fa69d1e9a273
kaisercode13 <alex.reinhardt22@gmail.com>
2022-11-28 13:09:04 +0100

    vue update

--- section-04-vue/my-first-vue-project/src/App.vue
+++ section-04-vue/my-first-vue-project/src/App.vue
@@ -1,20 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import MyFirstComponentVue from './components/vueBasics/MyFirstComponent.vue';
 </script>
 
 <template>
+  <div>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="My name is Alex" />
     </div>
   </header>
 
+
   <main>
-    <TheWelcome />
+    <MyFirstComponentVue />
   </main>
+</div>
+
 </template>
 
 <style scoped>
---
+++ section-04-vue/my-first-vue-project/src/components/vueBasics/TemplateComponent.vue
@@ -0,0 +1,14 @@
+<!-- Aquí es donde colocaremos nuestro HTMl -->
+
+<template>
+
+    
+</template>
+
+<!-- Aquí es donde colocaremos nuestro JS -->
+
+<script setup></script>
+
+<!-- Aquí es donde colocaremos nuestro CSS -->
+
+<style></style>
